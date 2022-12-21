f1138d2bf43735f2c2d4d8404d78b32dde7b19b3
fatih ozer <fatihozer246@hotmail.com>
2022-12-21 13:02:02 +0100

    first_push

---
+++ src/components/AboutMe.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/AboutMe.css";
+
+
+
+function AboutMe() {
+  return <div>About Me</div>;
+}
+
+export default AboutMe;
---
+++ src/components/LeftSide.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "../styles/LeftSide.css";
+import { Link } from "react-router-dom";
+import profilePhoto from "../img/profile.jpg";
+import cv from "../img/FatihOzerCVUpdated.pdf";
+import banner from "../img/sidebar2.jpg";
+
+function LeftSide() {
+  return (
+    <nav className="leftSide">
+      <img className="leftSideBanner" src={banner} alt="banner" />
+      <div className="leftSideBar">
+        <Link to="/">
+         {/*  <img src={profilePhoto} alt="profile" className="circlePhoto" /> */}
+        </Link>
+        <div>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#AboutMe">
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#Experience">
+                Experience
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#education">
+                Education
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#skills">
+                Skills
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#projects">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#personalia">
+                Personalia
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href={cv} download>
+                Download CV
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default LeftSide;
